2fcbda313f487f5e28408f928f641dd9bd236b70
Thawkster <tristan@nithon.com>
2020-03-06 23:16:18 -0800

    [ADD] tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+class LoginTestCase(TestCase):
+
+    def test_login(self):
+
+        # First check for the default behavior
+        response = self.client.get('/sekrit/')
+        self.assertRedirects(response, '/accounts/login/?next=/sekrit/')
+
+        # Then override the LOGIN_URL setting
+        with self.settings(LOGIN_URL='/other/login/'):
+            response = self.client.get('/sekrit/')
+            self.assertRedirects(response, '/other/login/?next=/sekrit/')
+
+class SignUpViewTests(TestCase):
+    def test_signup(self):
+        user = User.objects.create()
+
+        # When we make a request, we get a response back.
+        response = self.client.get('/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
--- workforce/tests.py
+++ workforce/tests.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-# Create your tests here.
+from workforce.models import Clown
+
+class ClownTestCase(TestCase):
+    def setUp(self):
+        Clown.objects.create(name="Jingle", age=30, description="Bouncer")
+
+    def test_clown(self):
+        jingles = Clown.objects.get(name="Jingle")
+
+class ClownListViewTests(TestCase):
+    def test_multiple_clowns(self):
+
+        Clown.objects.create(name="Fuller", age=100, description="Ringleader")
+        Clown.objects.create(name="Icky", age=40, description="Clown")
+
+        response = self.client.get('/workforce')
+
+        self.assertEqual(response.status_code, 301)
+
+        responses = response.context['clowns']
+        self.assertEqual(len(responses), 2)
+
+        self.assertQuerysetEqual(
+            responses,
+            ['<Clown: My Test Clown>', '<Clown: Another Test Clown>'],
+            ordered=False
+        )
